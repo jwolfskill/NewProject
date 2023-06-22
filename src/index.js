8f954a973f4e0f866906fe1f9e336b2f408798a7
jwolfskill <jud@wolfskillconsulting.com>
2023-06-22 14:20:51 -0400

    set up initial line chart

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+const json = [
+  {
+    "month": "Jan",
+    "Apples": 327,
+    "Peaches": 437,
+    "Pears": 111
+  },
+  {
+    "month": "Feb",
+    "Apples": 555,
+    "Peaches": 454,
+    "Pears": 222
+  },
+  {
+    "month": "Mar",
+    "Apples": 242,
+    "Peaches": 864,
+    "Pears": 343
+  },
+  {
+    "month": "Apr",
+    "Apples": 222,
+    "Peaches": 454,
+    "Pears": 434
+  }
+]
+
+const options = {
+  bindto: '#chart',
+  size: {
+    height: 500,
+    width: 1200
+  },
+  color: { pattern: ['#4287f5','#753b6b']},
+  data: {
+    json: json,
+    labels: true,
+    keys: { value: ['month','Apples','Peaches','Pears']},
+  },
+  axis: {
+    x: { type: 'month'}
+  }
+}
+const chart = c3.generate(options);
